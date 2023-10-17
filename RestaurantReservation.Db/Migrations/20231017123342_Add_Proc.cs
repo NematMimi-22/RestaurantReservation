a2cf28358e33a4626f80f5716615a221dc3d686d
Nemat Mimi <nematmimi01@gmail.com>
2023-10-17 15:37:02 +0300

    Add Procedure

---
+++ RestaurantReservation.Db/Migrations/20231017123342_Add_Proc.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantReservation.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Proc : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+        CREATE PROCEDURE GetCustomersWithLargeReservations
+            @minPartySize INT
+        AS
+        BEGIN
+            SELECT DISTINCT c.CustomerId, c.FirstName, c.LastName
+            FROM Customers c
+            INNER JOIN Reservations r ON c.CustomerId = r.CustomerId
+            WHERE r.PartySize > @minPartySize;
+        END
+    ");
+        }
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP PROCEDURE GetCustomersWithPartySizeGreaterThan");
+        }
+    }
+}
